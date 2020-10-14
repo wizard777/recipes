13e393ff4446a6ecaa6e2e3bc951f1dc8d9085a8
Kris <wizard777@tlen.pl>
2020-10-14 09:30:52 +0200

    Modified pipe category

--- src/app/core/pipes/category.pipe.ts
+++ src/app/core/pipes/category.pipe.ts
@@ -1,18 +1,40 @@
 import { Pipe, PipeTransform } from '@angular/core';
 
 @Pipe({
-  name: 'ingredients'
+  name: 'category'
 })
 export class CategoryPipe implements PipeTransform {
 
   transform(value: any): any {
+    if (value.length === 0) {
+      return value;
+    }
+
 
     let cats: any;
     for (const cat of value) {
+      // console.log((cat.ingredients  ));
+      // if (cat.ingredients === 'Occasional dishe') {
+      //   cats.push( cat);
+      // }
+
       cats = cat.ingredients.split(', ').join('\n');
-      console.log(cats);
+
+      // console.log(cats);
     }
     return cats;
+
+    // if (!category) {
+    //   return category;
+    // }
+    // console.log(category);
+
+    // return  category.filter((item, i ) =>
+    //       item.category === 'Main dish'
+    //    || item.category === 'Desserts'
+    //    || ( console.log(item.category === 'Occasional dishes'.indexOf(item) !== i))
+    //    );
+
 
   }
 
